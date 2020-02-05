3f30cc374f049dac0f51ef62d7ca20878116e76a
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-02-05 08:09:35 -0500

    many blobs draw

--- World.java
+++ World.java
@@ -14,17 +14,23 @@ import javax.swing.JPanel;
  * @author jword
  */
 public class World extends JPanel {
-    private ArrayList<Blob> blobs = new ArrayList<>();    
-    private ArrayList<Glob> globs = new ArrayList<>();    
+    //private ArrayList<Blob> blobs = new ArrayList<>();    
+    //private ArrayList<Glob> globs = new ArrayList<>();    
     private ArrayList<Sprite> sprites = new ArrayList<>();
 
     public World() {
-        sprites.add(new Blob(10,10));
+        for (int i = 0; i < 200; i++) {
+            int x = (int) (Math.random() * 800);
+            int y = (int) (Math.random() * 600);
+            sprites.add(new Blob(x,y));    
+        }        
     }
     
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(30, 30, 100,150);
+        for (Sprite sprite : sprites) {
+            sprite.draw(g);
+        }
     }
     
 }
